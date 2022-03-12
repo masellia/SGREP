074ff8ccdd7375834c25bb29c7ebaf0dab4a4f9c
masellia <88507034+masellia@users.noreply.github.com>
2022-03-12 16:07:12 +0100

    Add files via upload

---
+++ black hole solutions/EFT_BH_metric.m
@@ -0,0 +1,135 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{-1 + (2*M)/r - (128*M^3*(11*M - 8*r))/(r^10*\[CapitalLambda]^6) - 
+  2*(1 - (2*M)/r)*((64*M^5*(-484*M^3 + 1803*M^2*r - 1713*M*r^2 + 480*r^3)*
+      \[Chi]^2)/(11*r^12*(-2*M + r)^2*\[CapitalLambda]^6) - 
+    (73728*M^5*(3*M - 2*r)*\[Chi]^2)/(11*(2*M - r)*r^11*
+      \[CapitalLambda]T^6) - ((-106444800*M^9*r*\[CapitalLambda]^6 + 
+       85155840*M^8*r^2*\[CapitalLambda]^6 - 14515200*M^7*r^3*
+        \[CapitalLambda]^6 - 7112448*M^6*r^4*\[CapitalLambda]^6 - 
+       3459456*M^5*r^5*\[CapitalLambda]^6 - 1663200*M^4*r^6*
+        \[CapitalLambda]^6 - 784080*M^3*r^7*\[CapitalLambda]^6 - 
+       356400*M^2*r^8*\[CapitalLambda]^6 - 149688*M*r^9*\[CapitalLambda]^6 - 
+       49896*r^10*\[CapitalLambda]^6 + 77616000*M^10*\[CapitalLambda]T^6 - 
+       19219200*M^9*r*\[CapitalLambda]T^6 - 56770560*M^8*r^2*
+        \[CapitalLambda]T^6 + 5980800*M^7*r^3*\[CapitalLambda]T^6 + 
+       2930592*M^6*r^4*\[CapitalLambda]T^6 + 1425424*M^5*r^5*
+        \[CapitalLambda]T^6 + 685300*M^4*r^6*\[CapitalLambda]T^6 + 
+       323070*M^3*r^7*\[CapitalLambda]T^6 + 146850*M^2*r^8*
+        \[CapitalLambda]T^6 + 61677*M*r^9*\[CapitalLambda]T^6 + 
+       20559*r^10*\[CapitalLambda]T^6)*\[Chi]^2*(-1 + 3*Cos[\[Theta]]^2))/
+     (11550*M^3*r^13*\[CapitalLambda]^6*\[CapitalLambda]T^6) + 
+    (128*M^3*(11*M - 8*r)*((M^4*\[Chi]^2)/(r^3*(-2*M + r)) + 
+       (M^3*(M + r)*\[Chi]^2*(-1 + 3*Cos[\[Theta]]^2))/(2*r^4)))/
+     (r^9*(-2*M + r)*\[CapitalLambda]^6)) - 
+  (512*(-432*M^6*r*\[CapitalLambda]^6 + 121*M^7*\[CapitalLambda]T^6 - 
+     108*M^6*r*\[CapitalLambda]T^6)*\[Chi]^2*Sin[\[Theta]]^2)/
+   (11*r^13*\[CapitalLambda]^6*\[CapitalLambda]T^6) + 
+  (\[Chi]^2*(-2*M^5 - 3*M^4*r + M^3*r^2 + 6*M^5*Cos[\[Theta]]^2 + 
+     3*M^4*r*Cos[\[Theta]]^2 - 3*M^3*r^2*Cos[\[Theta]]^2 + 
+     4*M^4*r*Sin[\[Theta]]^2))/r^5, (1 - (2*M)/r)^(-1) - 
+  (128*(67*M^4 - 36*M^3*r))/((2*M - r)^2*r^8*\[CapitalLambda]^6) + 
+  (73728*M^3*\[Chi]*Cos[\[Theta]])/(r^9*\[CapitalLambda]m^6) + 
+  (2*((M^4*\[Chi]^2)/((2*M - r)*r^3) - ((-5*M^4*\[Chi]^2 + M^3*r*\[Chi]^2)*
+       (-1 + 3*Cos[\[Theta]]^2))/(2*r^4)))/(1 - (2*M)/r) + 
+  (2*((64*M^5*(2728*M^3 - 12687*M^2*r + 10914*M*r^2 - 2640*r^3)*\[Chi]^2)/
+      (11*r^12*(-2*M + r)^2*\[CapitalLambda]^6) + 
+     (73728*M^5*(21*M - 11*r)*\[Chi]^2)/(11*(2*M - r)*r^11*
+       \[CapitalLambda]T^6) - ((-2235340800*M^9*r*\[CapitalLambda]^6 + 
+        766402560*M^8*r^2*\[CapitalLambda]^6 + 14515200*M^7*r^3*
+         \[CapitalLambda]^6 + 7112448*M^6*r^4*\[CapitalLambda]^6 + 
+        3459456*M^5*r^5*\[CapitalLambda]^6 + 1663200*M^4*r^6*
+         \[CapitalLambda]^6 + 784080*M^3*r^7*\[CapitalLambda]^6 + 
+        356400*M^2*r^8*\[CapitalLambda]^6 + 149688*M*r^9*\[CapitalLambda]^6 + 
+        49896*r^10*\[CapitalLambda]^6 + 720720000*M^10*\[CapitalLambda]T^6 - 
+        583968000*M^9*r*\[CapitalLambda]T^6 + 220872960*M^8*r^2*
+         \[CapitalLambda]T^6 - 5980800*M^7*r^3*\[CapitalLambda]T^6 - 
+        2930592*M^6*r^4*\[CapitalLambda]T^6 - 1425424*M^5*r^5*
+         \[CapitalLambda]T^6 - 685300*M^4*r^6*\[CapitalLambda]T^6 - 
+        323070*M^3*r^7*\[CapitalLambda]T^6 - 146850*M^2*r^8*
+         \[CapitalLambda]T^6 - 61677*M*r^9*\[CapitalLambda]T^6 - 
+        20559*r^10*\[CapitalLambda]T^6)*\[Chi]^2*(-1 + 3*Cos[\[Theta]]^2))/
+      (11550*M^3*r^13*\[CapitalLambda]^6*\[CapitalLambda]T^6) + 
+     (128*(67*M^4 - 36*M^3*r)*((M^4*\[Chi]^2)/((2*M - r)*r^3) - 
+        ((-5*M^4*\[Chi]^2 + M^3*r*\[Chi]^2)*(-1 + 3*Cos[\[Theta]]^2))/
+         (2*r^4)))/((2*M - r)*r^9*\[CapitalLambda]^6)))/(1 - (2*M)/r), 
+ r^2 + (\[Chi]*((62208*M^4*Cos[\[Theta]])/(5*r^8) - 
+     (8192*M^3*Cos[\[Theta]])/r^7))/\[CapitalLambda]m^6 + 
+  \[Chi]^2*((M^3*(1 - 3*Cos[\[Theta]]^2))/r - (M^4*(1 + 3*Cos[2*\[Theta]]))/
+     r^2) + (\[Chi]^2*((-18432*M^6*(1 + 3*Cos[2*\[Theta]]))/r^10 + 
+     (488448*M^5*(1 + 3*Cos[2*\[Theta]]))/(55*r^9) - 
+     (13824*M^4*(1 + 3*Cos[2*\[Theta]]))/(25*r^8) - 
+     (16128*M^3*(1 + 3*Cos[2*\[Theta]]))/(55*r^7) - 
+     (3888*M^2*(1 + 3*Cos[2*\[Theta]]))/(25*r^6) - 
+     (576*M*(1 + 3*Cos[2*\[Theta]]))/(7*r^5) - (216*(1 + 3*Cos[2*\[Theta]]))/
+      (5*r^4) - (3888*(1 + 3*Cos[2*\[Theta]]))/(175*M*r^3) - 
+     (54*(1 + 3*Cos[2*\[Theta]]))/(5*M^2*r^2) - (108*(1 + 3*Cos[2*\[Theta]]))/
+      (25*M^3*r)))/\[CapitalLambda]T^6 + 
+  (\[Chi]^2*((7104*M^7*(1 + 3*Cos[2*\[Theta]]))/r^11 - 
+     (4544*M^6*(1 + 3*Cos[2*\[Theta]]))/r^10 - 
+     (72192*M^5*(1 + 3*Cos[2*\[Theta]]))/(55*r^9) + 
+     (5696*M^4*(1 + 3*Cos[2*\[Theta]]))/(25*r^8) + 
+     (19936*M^3*(1 + 3*Cos[2*\[Theta]]))/(165*r^7) + 
+     (1602*M^2*(1 + 3*Cos[2*\[Theta]]))/(25*r^6) + 
+     (712*M*(1 + 3*Cos[2*\[Theta]]))/(21*r^5) + (89*(1 + 3*Cos[2*\[Theta]]))/
+      (5*r^4) + (1602*(1 + 3*Cos[2*\[Theta]]))/(175*M*r^3) + 
+     (89*(1 + 3*Cos[2*\[Theta]]))/(20*M^2*r^2) + (89*(1 + 3*Cos[2*\[Theta]]))/
+      (50*M^3*r)))/\[CapitalLambda]^6, r^2*Sin[\[Theta]]^2 + 
+  (\[Chi]*((62208*M^4*Cos[\[Theta]]*Sin[\[Theta]]^2)/(5*r^8) - 
+     (8192*M^3*Cos[\[Theta]]*Sin[\[Theta]]^2)/r^7))/\[CapitalLambda]m^6 + 
+  \[Chi]^2*(-((M^3*(-1 + 3*Cos[\[Theta]]^2)*Sin[\[Theta]]^2)/r) - 
+    (M^4*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/r^2) + 
+  (\[Chi]^2*((-18432*M^6*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/r^10 + 
+     (488448*M^5*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(55*r^9) - 
+     (13824*M^4*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(25*r^8) - 
+     (16128*M^3*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(55*r^7) - 
+     (3888*M^2*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(25*r^6) - 
+     (576*M*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(7*r^5) - 
+     (216*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(5*r^4) - 
+     (3888*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(175*M*r^3) - 
+     (54*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(5*M^2*r^2) - 
+     (108*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(25*M^3*r)))/
+   \[CapitalLambda]T^6 + 
+  (\[Chi]^2*((7104*M^7*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/r^11 - 
+     (4544*M^6*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/r^10 - 
+     (72192*M^5*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(55*r^9) + 
+     (5696*M^4*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(25*r^8) + 
+     (19936*M^3*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(165*r^7) + 
+     (1602*M^2*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(25*r^6) + 
+     (712*M*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(21*r^5) + 
+     (89*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(5*r^4) + 
+     (1602*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(175*M*r^3) + 
+     (89*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(20*M^2*r^2) + 
+     (89*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(50*M^3*r)))/
+   \[CapitalLambda]^6, (-2*M^2*\[Chi]*Sin[\[Theta]]^2)/r + 
+  \[Chi]^3*((M^5*(-1 + 5*Cos[\[Theta]]^2)*Sin[\[Theta]]^2)/r^4 + 
+    (M^4*(-1 + 5*Cos[\[Theta]]^2)*Sin[\[Theta]]^2)/r^3 - 
+    (M^6*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/r^5) + 
+  (\[Chi]*((1408*M^5*Sin[\[Theta]]^2)/r^10 - (13824*M^4*Sin[\[Theta]]^2)/
+       (11*r^9)) + \[Chi]^3*(((8601 - 33215*Cos[\[Theta]]^2)*Sin[\[Theta]]^2)/
+       (1925*r^5) + (64*M^5*(20733 - 22675*Cos[\[Theta]]^2)*Sin[\[Theta]]^2)/
+       (10725*r^10) + (14144*M^9*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       r^14 - (482*(3 + 5*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(275*M*r^4) - 
+      (973*(3 + 5*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(550*M^2*r^3) - 
+      (128*M^8*(816 + 3295*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(11*r^13) - 
+      (192*M^7*(5281 + 6003*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(55*r^12) - 
+      (4*M*(2496 + 12971*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(5775*r^6) - 
+      (8*M^2*(-4798 + 17131*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(12705*r^7) + 
+      (512*M^6*(35999 + 87414*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/(3575*r^11) - 
+      (8*M^4*(-411207 + 460085*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (117975*r^9) - (2*M^3*(-377339 + 519795*Cos[2*\[Theta]])*
+        Sin[\[Theta]]^2)/(63525*r^8)))/\[CapitalLambda]^6 + 
+  ((-55296*M^4*\[Chi]*Sin[\[Theta]]^2)/(11*r^9) + 
+    \[Chi]^3*((-451584*M^8*(1 + 3*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (11*r^13) - (1642*(3 + 5*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (275*M*r^4) + (56*(3 + 5*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (275*M^2*r^3) + (4608*M^7*(691 + 1293*Cos[2*\[Theta]])*
+        Sin[\[Theta]]^2)/(55*r^12) + 
+      (13824*M^6*(4597 + 12267*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (3575*r^11) - (8*(12333 + 19565*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (1925*r^5) - (256*M^5*(18771 + 25825*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (3575*r^10) - (16*M*(39819 + 61019*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (5775*r^6) - (64*M^2*(42789 + 63692*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (12705*r^7) - (48*M^3*(178053 + 258035*Cos[2*\[Theta]])*
+        Sin[\[Theta]]^2)/(21175*r^8) - 
+      (64*M^4*(454917 + 642365*Cos[2*\[Theta]])*Sin[\[Theta]]^2)/
+       (39325*r^9)))/\[CapitalLambda]T^6}
